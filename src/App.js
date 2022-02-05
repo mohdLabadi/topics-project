20eda56967ccf7010c5a2bf64a92f700a0c2a573
Mohammad Allabadi <MohammadAllabadi22@kingsacademy.edu.jo>
2022-02-06 01:41:59 +0200

    trying out table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [xx, setxx] = useState(1);
 
-  let x =1;
+  let x = 1;
   return (
     <div className="App">
       <header className="App-header">
@@ -13,21 +13,43 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1 onClick={() => {setxx(1)}}>Click me again</h1>
-        <button onClick={()=> {setxx(xx+1)}}>press me</button>
+        <h1
+          onClick={() => {
+            setxx(1);
+          }}
+        >
+          Click me again
+        </h1>
+        <button
+          onClick={() => {
+            setxx(xx + 1);
+          }}
+        >
+          press me
+        </button>
         <h1>{xx}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          
-        </a>
+        ></a>
       </header>
-      <div className="class"><table><tr></tr><tr></tr><tr></tr></table></div>
+      <div className="class">
+
+        <table border="5px, dashed">
+          <tr>
+            <td>df</td>
+          </tr>
+          <tr>
+            <td>td</td>
+            <td>td</td>
+          </tr>
+          <tr><td>sd</td></tr>
+        </table>
+
+      </div>
     </div>
-    
   );
 }
 
