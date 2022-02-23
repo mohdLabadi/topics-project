cfa08ec7852321e91e1d38136bcea5c9ba66c992
noobylub <73588764+noobylub@users.noreply.github.com>
2022-02-23 02:04:06 +0200

    refreshed webpage, still need to work on the score updating.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,105 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import Hangman from "./options/Hangman";
+import Memory from "./options/memoryCard";
 
 function App() {
-  const [xx, setxx] = useState(1);
+  //input and finalized first and last names
+  const [secondName, setSecondName] = useState();
+  const [firstName, setFirstName] = useState();
 
-  let x = 1;
+  //what to display
+  const [display, setDisplay] = useState("logo");
+
+  //the scoring
+  const [oneScore, setOneScore] = useState(0);
+  const [twoScore, setTwoScore] = useState(0);
+  let game;
+
+  if (display == "logo") {
+    game = (
+      <img
+        src="https://a1.cdn.japantravel.com/photo/45058-216140/1440x960!/tokyo-your-name-real-life-locations-216140.jpg"
+        alt=""
+        className="img-fluid game"
+      />
+    );
+  } else if (display == "game-1") {
+    game = (
+      <Hangman
+        scoreOne={oneScore}
+        scoreTwo={twoScore}
+        changeFirst={setOneScore}
+        changeSecond={setTwoScore}
+        first={firstName}
+        second={secondName}
+      ></Hangman>
+    );
+  } else if (display == "game-2") {
+    game = (
+      <Memory
+        fscoreOne={oneScore}
+        scoreTwo={twoScore}
+        changeFirst={setOneScore}
+        changeSecond={setTwoScore}
+        first={firstName}
+        second={secondName}
+      ></Memory>
+    );
+  }
+
   return (
-    <div className="App">
-    <div className="header">
-        <h1>Tic Tac Toe</h1>
-        <div className="players">
-          <h3>Player One</h3>
-          <h3>Player Two</h3>
+    <div className="container-fluid all">
+      <h1 className="Jumbotron row text-center  main-text">
+        <div className="col-xs-12">NoobyLoob and LLabadi Games</div>
+      </h1>
+      <div className="row">
+        <div className="col-lg-2 players ">
+          <div className="player">
+            <h4>Player 1</h4>
+            <input
+              className="input-player"
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <h4>Score: {oneScore}</h4>
+          </div>
+          <div className="player"></div>
+
+          <h4>Player 2</h4>
+          <input
+            className="input-player"
+            type="text"
+            value={secondName}
+            onChange={(e) => {
+              setSecondName(e.target.value);
+            }}
+          />
+          <h4>Score: {twoScore}</h4>
+        </div>
+        <div className="col-lg-8 game">{game}</div>
+
+        <div className="col-lg-2 ">
+          <div className="buttons">
+            <button
+              onClick={() => {
+                setDisplay("game-1");
+              }}
+            >
+              Hangman
+            </button>
+            <button
+              onClick={() => {
+                setDisplay("game-2");
+              }}
+            >
+              Tic Tac Toe
+            </button>
+          </div>
         </div>
       </div>
-
-      
-      <div className="class">
-
-        <table>
-          <tr>
-            <td>tic</td>
-            <td>tac</td>
-            <td>toe</td>
-          </tr>
-          <tr>
-            <td>tic</td>
-            <td>tac</td>
-            <td>toe</td>
-          </tr>
-          <tr>
-            <td>sd</td>
-            <td>df</td>
-            <td>df</td>
-          </tr>
-        </table>
-
-      </div>
-
-      
-     
     </div>
   );
 }
