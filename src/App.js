f5909810ffd4a0d6313c89f94cba0360ff53e8f9
mohdLabadi <mohammadallabadi22@kingsacademy.edu.jo>
2022-02-23 14:27:21 +0200

    hi

--- src/App.js
+++ src/App.js
@@ -1,47 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
+
+
 
 function App() {
-  const [xx, setxx] = useState(1);
-
-  let x = 1;
+  
   return (
-    <div className="App">
-    <div className="header">
-        <h1>Tic Tac Toe</h1>
-        <div className="players">
-          <h3>Player One</h3>
-          <h3>Player Two</h3>
-        </div>
+    <div className="container">
+      <div className="jumbotron">
+        <h1> Memory Game </h1>
       </div>
-
-      
-      <div className="class">
-
-        <table>
-          <tr>
-            <td>tic</td>
-            <td>tac</td>
-            <td>toe</td>
-          </tr>
-          <tr>
-            <td>tic</td>
-            <td>tac</td>
-            <td>toe</td>
-          </tr>
-          <tr>
-            <td>sd</td>
-            <td>df</td>
-            <td>df</td>
-          </tr>
-        </table>
-
-      </div>
-
-      
-     
+      <table border="3px" className="table table-bordered">
+        <tr> <td>jk</td> <td>jk</td></tr>
+        <tr> <td>jk</td> <td>jk</td></tr>
+        <tr> <td>jk</td> <td>jk</td></tr>
+        <tr> <td>jk</td> <td>jk</td></tr>
+        <tr> klk</tr>
+        <tr> iouituf</tr>
+        <tr><button type="button" className="btn btn-primary">Primary</button></tr>
+      </table>
     </div>
+       
   );
 }
 
