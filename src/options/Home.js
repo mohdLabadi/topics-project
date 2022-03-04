884890a1a9de87f272a68fde0501031aa27325ed
noobylub <73588764+noobylub@users.noreply.github.com>
2022-03-05 02:14:28 +0300

    home page

--- src/options/Home.js
+++ src/options/Home.js
@@ -4,8 +4,11 @@ function HomePage(){
 
     return(
         <div className="container-fluid all-home">
-            <div className="row">
-                
+            <div className="row d-flex justify-content-center">
+                <div className="col-xs-6 main-content">
+                    <h1>Come and Play Games</h1>
+                    <p>See our two collection of games </p>
+                </div>
 
             </div>
             
